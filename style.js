2b5d4ead565120b085ce285c7b1e3e7b670c9f3c
TiKheoTay <tikheotayy@gmail.com>
2024-03-18 20:22:43 +0700

    olala

---
+++ style.js
@@ -0,0 +1,4 @@
+document.getElementById("user_infor").addEventListener("click",function(){alert("Button clicked!")});
+document.getElementById("user_infor").addEventListener("blur", function() {
+    validateInput();
+});

20bb680e8b923e4e38e3c934318fb581d0ccbeec
TiKheoTay <tikheotayy@gmail.com>
2024-03-21 20:47:29 +0700

    search

--- style.js
+++ style.js
@@ -1,3 +1,26 @@
 
-var list = document.getElementsByClass("test tí")
+var ok = document.querySelector(`.olala`);
+console.log(ok)
+ok.children
+console.log(ok.children)
+var list = ok.children
 console.log(list)
+for (let i=0;i<list.length;i++){
+    list[i].onclick = function(){
+        list[i].classList.toggle(`olala1`)
+    }
+}
+
+const searchIcon = document.getElementById(`oo`)
+const searchForm = document.getElementById(`ooo`)
+searchIcon.addEventListener(`click`, function(){
+    if(searchForm.style.display===`block`){
+        searchForm.display=`none`
+    }else{
+        searchForm.display=`block`
+    }
+})
+
+
+
+
